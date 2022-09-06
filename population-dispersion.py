b3f2770432aaca628b4ae78717a9fefd7e4a5ce5
mrberns1 <111442147+mrberns1@users.noreply.github.com>
2022-09-06 15:42:16 -0500

    Add files via upload

--- population-dispersion.py
+++ population-dispersion.py
@@ -1,1 +1,9 @@
 
+import statistics
+statistics.pvariance([1, 3, 4, 2, 6, 5, 3, 4, 5, 2])
+statistics.stdev([1, 3, 4, 2, 6, 5, 3, 4, 5, 2])
+
+print(statistics.pvariance([1, 3, 4, 2, 6, 5, 3, 4, 5, 2]))
+print(statistics.stdev([1, 3, 4, 2, 6, 5, 3, 4, 5, 2]))
+
+#missybernskoetter
